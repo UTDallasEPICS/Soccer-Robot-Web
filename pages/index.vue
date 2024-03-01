d02d7de85cecd3b938a59e07d09e22cde924174b
Daniel Nguyen <94246004+CrazEpic@users.noreply.github.com>
2024-03-01 16:09:47 -0600

    Initial button designs

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+div.flex.flex-nowrap
+    div.flex-auto.shrink-0
+        p LogoPlaceholder
+    div.flex.flex-nowrap.shrink-0.space-x-3
+        Button(style="background-color: #154734") {{ "Log In" }}
+        Button(style="background-color: #e87500") {{ "Sign Up" }}
+</template>
+
+<script setup lang="ts">
+</script>
