30f46f3f0d55d35412eb78e129cd465daee02f88
Daniel Nguyen <94246004+CrazEpic@users.noreply.github.com>
2024-03-02 23:16:28 -0600

    Basic SignUpOverlay and Sign Up button functionaliy

---
+++ components/SignUpOverlay.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+div.fixed.w-full.h-full.inset-0.bg-black.bg-opacity-80.flex(style="z-index: 2")
+    div.w-min.h-min.rounded-lg.p-2.bg-white.text-white(style="margin: auto;")
+        div.w-min.h-min.text-black(style="margin-left: auto;")
+            p.cursor-pointer(@click="emitClose") X
+        form(@submit.prevent="handleSubmit")
+            label.text-black Username
+            input.text-black.border-2.border-black(type="text" v-model="username" required)
+</template>
+
+<script setup lang="ts">
+const username = ref("")
+const handleSubmit = () => {
+    console.log(username.value)
+}
+const emit = defineEmits(['close'])
+const emitClose = () => {
+    emit('close')
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,12 @@ div.flex.flex-nowrap
         p LogoPlaceholder
     div.flex.flex-nowrap.shrink-0.space-x-3
         Button(style="background-color: #154734") {{ "Log In" }}
-        Button(style="background-color: #e87500") {{ "Sign Up" }}
+        Button(@click="openSignUp" style="background-color: #e87500") {{ "Sign Up" }}
+SignUpOverlay(v-if="showSignUp" @close="closeSignUp")
 </template>
 
 <script setup lang="ts">
+const showSignUp = ref(false)
+const openSignUp = () => { showSignUp.value = true }
+const closeSignUp = () => { showSignUp.value = false }
 </script>
