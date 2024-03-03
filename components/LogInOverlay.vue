25c426803aca657c60f2839ad01df139d22fc25e
Daniel Nguyen <94246004+CrazEpic@users.noreply.github.com>
2024-03-03 11:28:02 -0600

    Added headers and overlays, set specific overlay dimensions
    Added headers and overlays for About Us, How to Play, and Help
    Set specific overlay width and height

--- components/LogInOverlay.vue
+++ components/LogInOverlay.vue
@@ -3,7 +3,7 @@ div.fixed.w-full.h-full.inset-0.bg-black.bg-opacity-80.flex(style="z-index: 2")
     div.w-min.h-min.rounded-lg.p-2.bg-white(style="margin: auto;")
         div.w-min.h-min.text-black(style="margin-left: auto;")
             p.cursor-pointer(@click="emitClose") X
-        p.text-black Sign Up
+        p.text-black Log In
         form(@submit.prevent="handleSubmit")
             label.text-black Username
             input.text-black.border-2.border-black(type="text" v-model="username" required)
@@ -14,8 +14,8 @@ const username = ref("")
 const handleSubmit = () => {
     console.log(username.value)
 }
-const emit = defineEmits(['closeSignUpOverlay'])
+const emit = defineEmits(['closeLogInOverlay'])
 const emitClose = () => {
-    emit('closeSignUpOverlay')
+    emit('closeLogInOverlay')
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,36 @@ div.flex.flex-nowrap
     div.flex-auto.shrink-0
         p LogoPlaceholder
     div.flex.flex-nowrap.shrink-0.space-x-3
-        Button(style="background-color: #154734") {{ "Log In" }}
+        Button(@click="openLogIn" style="background-color: #154734") {{ "Log In" }}
         Button(@click="openSignUp" style="background-color: #e87500") {{ "Sign Up" }}
-SignUpOverlay(v-if="showSignUp" @close="closeSignUp")
+div.flex.flex-nowrap.justify-evenly.text-white.text-2xl.font-bold(style="background-color: #e87500")
+    div
+        p.cursor-pointer(@click="openAboutUs") About Us
+    div
+        p.cursor-pointer(@click="openHowToPlay") How to Play
+    div
+        p.cursor-pointer(@click="openHelp") Help
+SignUpOverlay(v-if="showSignUp" @closeSignUpOverlay="closeSignUp")
+LogInOverlay(v-if="showLogIn" @closeLogInOverlay="closeLogIn")
+AboutUsOverlay(v-if="showAboutUs" @closeAboutUsOverlay="closeAboutUs")
+HowToPlayOverlay(v-if="showHowToPlay" @closeHowToPlayOverlay="closeHowToPlay")
+HelpOverlay(v-if="showHelp" @closeHelpOverlay="closeHelp")
 </template>
 
 <script setup lang="ts">
 const showSignUp = ref(false)
+const showLogIn = ref(false)
+const showAboutUs = ref(false)
+const showHowToPlay = ref(false)
+const showHelp = ref(false)
 const openSignUp = () => { showSignUp.value = true }
 const closeSignUp = () => { showSignUp.value = false }
+const openLogIn = () => { showLogIn.value = true }
+const closeLogIn = () => { showLogIn.value = false }
+const openAboutUs = () => { showAboutUs.value = true }
+const closeAboutUs = () => { showAboutUs.value = false }
+const openHowToPlay = () => { showHowToPlay.value = true }
+const closeHowToPlay = () => { showHowToPlay.value = false }
+const openHelp = () => { showHelp.value = true }
+const closeHelp = () => { showHelp.value = false }
 </script>
