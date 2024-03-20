ab5815f1013f8053532559b3ad2ce32db95f18a5
Max Lichter <iammaxlichter@gmail.com>
2024-03-20 16:32:25 -0500

    Componenting

---
+++ components/LogInSignUp.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="flex md:flex md:flex-grow flex-row-reverse space-x-3 mt-5 mr-4 mb-2">
+        <div class="">
+            <button
+                class="hover:text-gray-300 transition font-semibold duration-400 text-white font-bold py-2 px-5 rounded-lg"
+                style="background-color:#154734" @click="openLogIn"> {{ "Log In" }}</button>
+        </div>
+        <div class="">
+            <button
+                class="hover:text-gray-300 transition font-semibold duration-400 text-white font-bold py-2 px-5 mr-5 rounded-lg"
+                style="background-color:#e87500" @click="openSignUp"> {{ "Sign Up" }}</button>
+        </div>
+    </div>
+    <SignUpOverlay v-if="showSignUp" @closeSignUpOverlay="closeSignUp"></SignUpOverlay>
+    <LogInOverlay v-if="showLogIn" @closeLogInOverlay="closeLogIn"></LogInOverlay>
+</template>
+
+<script setup lang="ts">
+const showSignUp = ref(false)
+const showLogIn = ref(false)
+const openSignUp = () => { showSignUp.value = true }
+const closeSignUp = () => { showSignUp.value = false }
+const openLogIn = () => { showLogIn.value = true }
+const closeLogIn = () => { showLogIn.value = false }
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,5 @@
 <template>
-<div class = "flex flex-nowrap shrink-0 space-x-3">
-    <div class="utdLogo" style="width:450px">
-            <img src="/pages/UTDLogo.svg">
-    </div>
-    <div class="flex md:flex md:flex-grow flex-row justify-end space-x-3 mr-4 mt-5 mb-2">
-        <div class="">
-            <button class="hover:text-gray-300 transition font-semibold duration-400 text-white font-bold py-2 px-5 rounded-lg" style="background-color:#154734" @click="openLogIn"> {{ "Log In" }}</button>
-        </div>
-        <div class=""> 
-            <button class="hover:text-gray-300 transition font-semibold duration-400 text-white font-bold py-2 px-5 mr-5 rounded-lg" style="background-color:#e87500" @click="openSignUp"> {{ "Sign Up" }}</button>
-        </div>
-    </div>
-</div>
+    <TopNavBar></TopNavBar>
 
 <div class = "flex flex-nowrap justify-evenly text-white text-2x1 font-bold py-3" style = "background-color: #e87500">
     <div>
@@ -25,23 +13,16 @@
     </div>
 </div>
 
-<SignUpOverlay v-if="showSignUp" @closeSignUpOverlay = "closeSignUp"></SignUpOverlay>
-<LogInOverlay v-if="showLogIn" @closeLogInOverlay = "closeLogIn"></LogInOverlay>
+
 <AboutUsOverlay v-if="showAboutUs" @closeAboutUsOverlay = "closeAboutUs"></AboutUsOverlay>
 <HowToPlayOverlay v-if="showHowToPlay" @closeHowToPlayOverlay = "closeHowToPlay"></HowToPlayOverlay>
 <HelpOverlay v-if="showHelp" @closeHelpOverlay = "closeHelp"></HelpOverlay>
 </template>
 
 <script setup lang="ts">
-const showSignUp = ref(false)
-const showLogIn = ref(false)
 const showAboutUs = ref(false)
 const showHowToPlay = ref(false)
 const showHelp = ref(false)
-const openSignUp = () => { showSignUp.value = true }
-const closeSignUp = () => { showSignUp.value = false }
-const openLogIn = () => { showLogIn.value = true }
-const closeLogIn = () => { showLogIn.value = false }
 const openAboutUs = () => { showAboutUs.value = true }
 const closeAboutUs = () => { showAboutUs.value = false }
 const openHowToPlay = () => { showHowToPlay.value = true }
