a0aef18bffc5847dc12a694337c5bcd2ce39f171
Daniel Nguyen <94246004+CrazEpic@users.noreply.github.com>
2024-03-20 19:34:26 -0500

    Forgot to update index.vue

--- components/Button.vue
+++
@@ -1,7 +0,0 @@
-<template lang="pug">
-button.rounded-lg.py-1.px-3.text-white.font-bold
-    slot
-</template>
-
-<script setup lang="ts">
-</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,111 @@
-<template lang="pug">
-div.flex.flex-nowrap
-    div.flex-auto.shrink-0
-        p LogoPlaceholder
-    div.flex.flex-nowrap.shrink-0.space-x-3
-        Button(@click="openLogIn" style="background-color: #154734") {{ "Log In" }}
-        Button(@click="openSignUp" style="background-color: #e87500") {{ "Sign Up" }}
-div.flex.flex-nowrap.justify-evenly.text-white.text-2xl.font-bold(style="background-color: #e87500")
-    div
-        p.cursor-pointer(@click="openAboutUs") About Us
-    div
-        p.cursor-pointer(@click="openHowToPlay") How to Play
-    div
-        p.cursor-pointer(@click="openHelp") Help
-SignUpOverlay(v-if="showSignUp" @closeSignUpOverlay="closeSignUp")
-LogInOverlay(v-if="showLogIn" @closeLogInOverlay="closeLogIn")
-AboutUsOverlay(v-if="showAboutUs" @closeAboutUsOverlay="closeAboutUs")
-HowToPlayOverlay(v-if="showHowToPlay" @closeHowToPlayOverlay="closeHowToPlay")
-HelpOverlay(v-if="showHelp" @closeHelpOverlay="closeHelp")
+<template>
+<p>Hello World</p>
+<div>
+    <button @click="listenForKey" class="border-black border-2"> Start Key Listen </button>
+    <button @click="stopListeningForKey" class="border-black border-2"> Stop Key Listen </button>
+</div>
+<div>
+    <p>Queue: {{ queue }}</p>
+    <button @click="joinQueue" class="border-black border-2"> Join Queue </button>
+    <button @click="leaveQueue" class="border-black border-2"> Leave Queue </button>
+</div>
+<div>
+    <p>Timer: {{ timer }}</p>
+    <p>Player 1 Score: {{ player1Score }}</p>
+    <p>Player 2 Score: {{ player2Score }}</p>
+</div>
+<p>Current Username: {{ username }}</p>
+<input v-model="username" placeholder="edit me" class="border-black border-2"/>
 </template>
 
 <script setup lang="ts">
-const showSignUp = ref(false)
-const showLogIn = ref(false)
-const showAboutUs = ref(false)
-const showHowToPlay = ref(false)
-const showHelp = ref(false)
-const openSignUp = () => { showSignUp.value = true }
-const closeSignUp = () => { showSignUp.value = false }
-const openLogIn = () => { showLogIn.value = true }
-const closeLogIn = () => { showLogIn.value = false }
-const openAboutUs = () => { showAboutUs.value = true }
-const closeAboutUs = () => { showAboutUs.value = false }
-const openHowToPlay = () => { showHowToPlay.value = true }
-const closeHowToPlay = () => { showHowToPlay.value = false }
-const openHelp = () => { showHelp.value = true }
-const closeHelp = () => { showHelp.value = false }
+const props = defineProps(["message"])
+const ws = ref<WebSocket>()
+const queue = ref([])
+const timer = ref(0)
+const player1Score = ref(0)
+const player2Score = ref(0)
+const username = ref("")
+
+onMounted(() => {
+    console.log("Component mounted")
+    ws.value = new WebSocket("ws://localhost:8080")
+    ws.value.onerror = (event) => {
+        console.log("Error: " + event)
+    }
+    ws.value.onopen = (event) => {
+        console.log("WebSocket connection established")
+        if(ws.value){
+            ws.value.send(JSON.stringify({
+                type: "REQUESTQUEUE",
+                payload: ""
+            }))
+            ws.value.send(JSON.stringify({
+                type: "REQUESTTIMER",
+                payload: ""
+            }))
+            ws.value.send(JSON.stringify({
+                type: "REQUESTSCORE",
+                payload: ""
+            }))
+        }
+    }
+    ws.value.onclose = (event) => {
+        console.log("WebSocket connection closed")
+    }
+    ws.value.onmessage = (event) => {
+        const { type, payload } = JSON.parse(event.data)
+        console.log(`Received message => ${type} : ${payload}`)
+        if (type === "QUEUEUPDATE") {
+            queue.value = payload
+        }
+        else if (type === "TIMERUPDATE") {
+            timer.value = payload
+        }
+        else if (type === "SCOREUPDATE") {
+            player1Score.value = payload.player1
+            player2Score.value = payload.player2
+        }
+    }
+})
+
+const logKey = (event: KeyboardEvent) => {
+    console.log("Key pressed " + event.key)
+    if(ws.value) {
+        let message = {
+            type: "KEY",
+            payload: event.key
+        }
+        ws.value.send(JSON.stringify(message))
+    }
+}
+
+const listenForKey = () => {
+    window.addEventListener("keyup", logKey)
+}
+
+const stopListeningForKey = () => {
+    window.removeEventListener("keyup", logKey)
+}
+
+const joinQueue = () => {
+    if(ws.value) {
+        let message = {
+            type: "QUEUEJOIN",
+            payload: username.value
+        }
+        ws.value.send(JSON.stringify(message))
+    }
+}
+
+const leaveQueue = () => {
+    if(ws.value) {
+        let message = {
+            type: "QUEUELEAVE",
+            payload: username.value
+        }
+        ws.value.send(JSON.stringify(message))
+    }
+}
+
 </script>
