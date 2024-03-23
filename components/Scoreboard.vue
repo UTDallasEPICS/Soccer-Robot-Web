fe050d4f01ecec6d75eaedec1b72ae84f9435bdc
Jahnzeb Bastaki <jahnzebbastaki@gmail.com>
2024-03-23 17:16:51 -0500

    Finished VideoStream component

---
+++ components/Scoreboard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="h-16 bg-teal-200 scoreboard-container">
+    Hello
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style>
+  .scoreboard-container{
+    width: 888px;
+    height: 74px;
+  }
+</style>
---
+++ components/VideoStream.vue
@@ -0,0 +1,13 @@
+<template>
+  <iframe :src="src" frameborder="0" allowfullscreen="false" scrolling="no" :height="height" :width="width"></iframe>
+</template>
+
+<script setup lang="ts">
+  const src = `https://player.twitch.tv/?channel=${process.env.CHANNELNAME}&parent=${process.env.PARENTNAME}` 
+  const height = 500
+  const width = 888
+</script>
+
+<style>
+
+</style>
