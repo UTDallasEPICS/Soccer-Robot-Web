fd735d773d374eb7361f686fc7d81c013062897b
Max Lichter <iammaxlichter@gmail.com>
2024-04-10 15:53:59 -0500

    made the shit

--- components/Scoreboard.vue
+++ components/Scoreboard.vue
@@ -1,13 +1,12 @@
 <template>
-    
-  <div class="border-4 rounded-2xl border-black flex justify-between items-center overflow-hidden" style="width: 888px; height: 84px; margin-top: 0%;">
-      <p class="border-r-4 rounded-r-none border-black rounded-2xl bg-auto" style="font-family: Jockey One; font-weight: 400; font-size: 60px; background-color: #E87500 ;width: 130px; text-align: center;">{{ user1score }}</p>
-      <p class="basis-0 grow text-center" style="font-family: Helvetica; font-weight: bold; font-size: 30px">{{user1}}</p>
-      <div class="bg-slate-300 w-min border-black border-l-4 border-r-4 h-full px-3 h-full flex items-center" >
-        <p class="" style="font-family: Helvetica; font-weight: bold; font-size: 30px">{{ timer }}</p>
-      </div>
-      <p class="basis-0 grow text-center" style="font-family: Helvetica; font-weight: bold; font-size: 30px">{{user2}}</p>
-      <p class="border-l-4 rounded-l-none border-black rounded-2xl bg-auto" style="font-family: Jockey One; font-weight: 400; font-size: 60px; background-color: #5FE0B7; width: 130px; text-align: center;">{{ user2score }}</p>
+  <div class="border-4 rounded-lg border-black flex justify-between items-center overflow-hidden" style="width: 959px; height: 69px; margin-top: 0%;">
+    <p class="border-r-4 rounded-r-none border-black rounded-lg bg-auto" style="font-family: Jockey One; font-weight: 400; font-size: 60px; background-color: #E87500 ;width: 130px; text-align: center;">{{ user1score }}</p>
+    <p class="basis-0 grow text-center" style="font-family: Helvetica; font-weight: bold; font-size: 30px">{{user1}}</p>
+    <div class="bg-slate-300 w-min border-black border-l-4 border-r-4 h-full px-3 h-full flex items-center">
+      <p class="" style="font-family: Helvetica; font-weight: bold; font-size: 30px">{{ timer }}</p>
+    </div>
+    <p class="basis-0 grow text-center" style="font-family: Helvetica; font-weight: bold; font-size: 30px">{{user2}}</p>
+    <p class="border-l-4 rounded-l-none border-black rounded-lg bg-auto" style="font-family: Jockey One; font-weight: 400; font-size: 60px; background-color: #5FE0B7; width: 130px; text-align: center;">{{ user2score }}</p>
   </div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,25 @@
 <template>
-    <TopNavBar></TopNavBar>
-    <BottomNavBar></BottomNavBar>
-    <QueueContainer></QueueContainer>
+    <div class="flex flex-col">
+        <div>
+            <TopNavBar></TopNavBar>
+            <BottomNavBar></BottomNavBar>
+        </div>
+        <div class="flex justify-center py-6">
+            <div class="flex flex-col">
+                <Scoreboard></Scoreboard>
+                <span class="py-4 pr-6">
+                    <VideoStream></VideoStream>
+                </span>
+            </div>
+            <QueueContainer></QueueContainer>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
 
 </script>
+
+<style scoped>
+/* Add any scoped styles here */
+</style>
