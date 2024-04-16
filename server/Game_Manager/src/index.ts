df88e1b35be848fc9e1ea5e4dfc540e2ccb627e9
Daniel Nguyen <94246004+CrazEpic@users.noreply.github.com>
2024-04-16 01:02:34 -0500

    Simulate game cycles: NOT_PLAYING, SEND_CONFIRM, PLAYING, RESETTING

--- server/Game_Manager/src/index.ts
+++ server/Game_Manager/src/index.ts
@@ -7,7 +7,10 @@ dotenv.config({ path: "./.env" })
 const PORT_SSE_GM: number = parseInt(`${process.env.PORT_SSE_GM}`)
 const PORT_GM_RASPBERRY = parseInt(`${process.env.PORT_GM_RASPBERRY}`)
 const PORT_CLIENT_GM = parseInt(`${process.env.PORT_CLIENT_GM}`)
+const PORT_EXPRESS_CONTROLLER_GAMEMANAGER = parseInt(`${process.env.PORT_EXPRESS_CONTROLLER_GAMEMANAGER}`)
+// TODO: RANDOMIZE THESE PASSWORDS EVERY REQUEST ITERATION
 let CONFIRMATION_PASSWORD: string = process.env.CONFIRMATION_PASSWORD ?? ""
+let CONTROLLER_ACCESS: string = process.env.CONTROLLER_ACCESS ?? ""
 
 // SHARED VARIABLES
 const queue: Array<{username: string, ws: any}> = []
@@ -15,6 +18,120 @@ const players: Array<{username: string, ws: any, accepted: boolean}> = []
 let timer: number = 0
 let score1: number = 0
 let score2: number = 0
+enum GAME_STATE { NOT_PLAYING, SEND_CONFIRM, PLAYING, RESETTING }
+let game_state: GAME_STATE = GAME_STATE.NOT_PLAYING
+
+// GAME CYCLES
+const gameCycle = setInterval(() => {
+    if(game_state == GAME_STATE.NOT_PLAYING){
+        // Check for sufficient users in queue to send confirmation request
+        if(queue.length >= 2){
+            game_state = GAME_STATE.SEND_CONFIRM
+            queue[0].ws.send(JSON.stringify({
+                "type": "MATCH_CONFIRMATION",
+                "payload": CONFIRMATION_PASSWORD
+            }))
+            queue[1].ws.send(JSON.stringify({
+                "type": "MATCH_CONFIRMATION",
+                "payload": CONFIRMATION_PASSWORD
+            }))
+        }
+    }
+    else if(game_state == GAME_STATE.SEND_CONFIRM){
+        // Check if received 2 confirmation response
+        // TODO: Set a confirmation timer of 15 seconds
+        if(players.length == 2){ // 2 confirmations
+            // 2 accepts -> start game
+            if(players[0]["accepted"] && players[1]["accepted"]){
+                game_state = GAME_STATE.PLAYING
+                console.log(players[0]["username"] + " vs " + players[1]["username"])
+                
+                // authorize players in Controller server to send key inputs
+                fetch(`http://localhost:${PORT_EXPRESS_CONTROLLER_GAMEMANAGER}/adduser`, {
+                    method: "POST",
+                    headers: {"Content-Type": "application/json"},
+                    body: JSON.stringify({
+                        "playernumber": 0,
+                        "username": players[0]["username"]
+                    })
+                }).then(() => {
+                    fetch(`http://localhost:${PORT_EXPRESS_CONTROLLER_GAMEMANAGER}/adduser`, {
+                        method: "POST",
+                        headers: {"Content-Type": "application/json"},
+                        body: JSON.stringify({
+                            "playernumber": 1,
+                            "username": players[1]["username"]
+                        })
+                    }).then(() => { // give players the access code to connect to Controller server WebSocket
+                        queue[0].ws.send(JSON.stringify({
+                            "type": "MATCH_START",
+                            "payload": CONTROLLER_ACCESS
+                        }))
+                        queue[1].ws.send(JSON.stringify({
+                            "type": "MATCH_START",
+                            "payload": CONTROLLER_ACCESS
+                        }))
+                        // close ws because players are not in queue anymore
+                        queue[0]["ws"].close()
+                        queue[1]["ws"].close()
+                        queue.splice(0, 2)
+                        timer = 60
+                        // yay
+                    })
+                })
+            }
+            else{
+                // did not get 2 accepts
+                // find the player(s) that declined and remove from queue/close ws connection
+                const indexA: number = queue.findIndex((element) => { return element["username"] === players[0]["username"]})
+                const indexB: number = queue.findIndex((element) => { return element["username"] === players[1]["username"]})
+                if(players[0]["accepted"] === false && players[1]["accepted"] === false){
+                    queue[0]["ws"].close()
+                    queue[1]["ws"].close()
+                    queue.splice(0, 2)
+                }
+                else{
+                    const indexToRemove = (players[0]["accepted"]) ? indexB : indexA
+                    queue[indexToRemove]["ws"].close()
+                    // reset the confirmation of the other player
+                    queue[1 - indexToRemove]["ws"].send(JSON.stringify({"type": "MATCH_CONFIRMATION_RESET", "payload": ""}))
+                    queue.splice(indexToRemove, 1)
+                }
+                game_state = GAME_STATE.NOT_PLAYING
+                players.splice(0, players.length) // clear array
+            }
+        }
+    }
+    else if(game_state == GAME_STATE.PLAYING){
+        // Check when timer reaches 0
+        console.log(`TIMER: ${timer} | ${players[0]["username"]} vs ${players[1]["username"]}`)
+        timer--
+        if(timer == 0){
+            game_state = GAME_STATE.RESETTING
+        }
+    }
+    else if(game_state == GAME_STATE.RESETTING){
+        // Game end: remove players from authorization in Controller server and clear player array
+        fetch(`http://localhost:${PORT_EXPRESS_CONTROLLER_GAMEMANAGER}/removeuser`, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                "playernumber": 0,
+                "username": players[0]["username"]
+            })
+        })
+        fetch(`http://localhost:${PORT_EXPRESS_CONTROLLER_GAMEMANAGER}/removeuser`, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                "playernumber": 0,
+                "username": players[1]["username"]
+            })
+        })
+        players.splice(0, 2)
+        game_state = GAME_STATE.NOT_PLAYING
+    }
+}, 1000)
 
 // WEBSOCKET LOGGED IN CLIENT <-> GAME MANAGER
 const wss_client_gm = new WebSocketServer({ port: PORT_CLIENT_GM })
