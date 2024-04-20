d31633351298fe9f157ff440cbb8d4283526aa19
Daniel Nguyen <94246004+CrazEpic@users.noreply.github.com>
2024-04-20 09:48:42 -0500

    Add server-side confirmation timer

--- server/Game_Manager/src/index.ts
+++ server/Game_Manager/src/index.ts
@@ -16,13 +16,14 @@ const PORT_SSE_GM: number = parseInt(`${process.env.PORT_SSE_GM}`)
 const PORT_GM_RASPBERRY = parseInt(`${process.env.PORT_GM_RASPBERRY}`)
 const PORT_CLIENT_GM = parseInt(`${process.env.PORT_CLIENT_GM}`)
 const PORT_EXPRESS_CONTROLLER_GAMEMANAGER = parseInt(`${process.env.PORT_EXPRESS_CONTROLLER_GAMEMANAGER}`)
-let CONFIRMATION_PASSWORD: string = process.env.CONFIRMATION_PASSWORD ?? ""
-let CONTROLLER_ACCESS: string = process.env.CONTROLLER_ACCESS ?? ""
 
 // SHARED VARIABLES
 const queue: Array<{username: string, ws: any}> = []
 const players: Array<{username: string, ws: any, accepted: boolean}> = []
+let CONFIRMATION_PASSWORD: string = "sousounofrieren" // "Tearful goodbyes aren’t our style. It’d be embarrassing when we meet again"
+let CONTROLLER_ACCESS: string = "donutvampire" // the initial value does not do anything here
 let timer: number = 0
+let confirmation_timer: number = 0
 let score1: number = 0
 let score2: number = 0
 enum GAME_STATE { NOT_PLAYING, SEND_CONFIRM, PLAYING, RESETTING }
@@ -43,82 +44,85 @@ const gameCycle = setInterval(() => {
                 "type": "MATCH_CONFIRMATION",
                 "payload": CONFIRMATION_PASSWORD
             }))
+            confirmation_timer = 15 // 15 seconds to confirm
         }
     }
     else if(game_state == GAME_STATE.SEND_CONFIRM){
         // Check if received 2 confirmation response
-        // TODO: Set a confirmation timer of 15 seconds
-        if(players.length == 2){ // 2 confirmations
-            // 2 accepts -> start game
-            if(players[0]["accepted"] && players[1]["accepted"]){
-                game_state = GAME_STATE.PLAYING
-                CONTROLLER_ACCESS = nanoid() // new access code for each game
-                // tell Controller server to change access code
-                fetch(`http://localhost:${PORT_EXPRESS_CONTROLLER_GAMEMANAGER}/accesspassword`, {
-                    method: "POST",
-                    headers: {"Content-Type": "application/json"},
-                    body: JSON.stringify({
-                        "accesspassword": CONTROLLER_ACCESS
-                    })
-                }).then(() => {
-                    console.log(players[0]["username"] + " vs " + players[1]["username"])
-                    
-                    // authorize players in Controller server to send key inputs
-                    fetch(`http://localhost:${PORT_EXPRESS_CONTROLLER_GAMEMANAGER}/adduser`, {
+        if(confirmation_timer == 0){ // time's up
+            if(players.length == 2){ // 2 confirmations
+                // 2 accepts -> start game
+                if(players[0]["accepted"] && players[1]["accepted"]){
+                    game_state = GAME_STATE.PLAYING
+                    CONTROLLER_ACCESS = nanoid() // new access code for each game
+                    // tell Controller server to change access code
+                    fetch(`http://localhost:${PORT_EXPRESS_CONTROLLER_GAMEMANAGER}/accesspassword`, {
                         method: "POST",
                         headers: {"Content-Type": "application/json"},
                         body: JSON.stringify({
-                            "playernumber": 0,
-                            "username": players[0]["username"]
+                            "accesspassword": CONTROLLER_ACCESS
                         })
                     }).then(() => {
+                        console.log(players[0]["username"] + " vs " + players[1]["username"])
+                        
+                        // authorize players in Controller server to send key inputs
                         fetch(`http://localhost:${PORT_EXPRESS_CONTROLLER_GAMEMANAGER}/adduser`, {
                             method: "POST",
                             headers: {"Content-Type": "application/json"},
                             body: JSON.stringify({
-                                "playernumber": 1,
-                                "username": players[1]["username"]
+                                "playernumber": 0,
+                                "username": players[0]["username"]
                             })
-                        }).then(() => { // give players the access code to connect to Controller server WebSocket
-                            queue[0].ws.send(JSON.stringify({
-                                "type": "MATCH_START",
-                                "payload": CONTROLLER_ACCESS
-                            }))
-                            queue[1].ws.send(JSON.stringify({
-                                "type": "MATCH_START",
-                                "payload": CONTROLLER_ACCESS
-                            }))
-                            // close ws because players are not in queue anymore
-                            queue[0]["ws"].close()
-                            queue[1]["ws"].close()
-                            queue.splice(0, 2)
-                            timer = 60
-                            // yay
+                        }).then(() => {
+                            fetch(`http://localhost:${PORT_EXPRESS_CONTROLLER_GAMEMANAGER}/adduser`, {
+                                method: "POST",
+                                headers: {"Content-Type": "application/json"},
+                                body: JSON.stringify({
+                                    "playernumber": 1,
+                                    "username": players[1]["username"]
+                                })
+                            }).then(() => { // give players the access code to connect to Controller server WebSocket
+                                queue[0].ws.send(JSON.stringify({
+                                    "type": "MATCH_START",
+                                    "payload": CONTROLLER_ACCESS
+                                }))
+                                queue[1].ws.send(JSON.stringify({
+                                    "type": "MATCH_START",
+                                    "payload": CONTROLLER_ACCESS
+                                }))
+                                // close ws because players are not in queue anymore
+                                queue[0]["ws"].close()
+                                queue[1]["ws"].close()
+                                queue.splice(0, 2)
+                                timer = 60
+                                // yay
+                            })
                         })
                     })
-                })
-            }
-            else{
-                // did not get 2 accepts
-                // find the player(s) that declined and remove from queue/close ws connection
-                const indexA: number = queue.findIndex((element) => { return element["username"] === players[0]["username"]})
-                const indexB: number = queue.findIndex((element) => { return element["username"] === players[1]["username"]})
-                if(players[0]["accepted"] === false && players[1]["accepted"] === false){
-                    queue[0]["ws"].close()
-                    queue[1]["ws"].close()
-                    queue.splice(0, 2)
                 }
                 else{
-                    const indexToRemove = (players[0]["accepted"]) ? indexB : indexA
-                    queue[indexToRemove]["ws"].close()
-                    // reset the confirmation of the other player
-                    queue[1 - indexToRemove]["ws"].send(JSON.stringify({"type": "MATCH_CONFIRMATION_RESET", "payload": ""}))
-                    queue.splice(indexToRemove, 1)
+                    // did not get 2 accepts
+                    // find the player(s) that declined and remove from queue/close ws connection
+                    const indexA: number = queue.findIndex((element) => { return element["username"] === players[0]["username"]})
+                    const indexB: number = queue.findIndex((element) => { return element["username"] === players[1]["username"]})
+                    if(players[0]["accepted"] === false && players[1]["accepted"] === false){
+                        queue[0]["ws"].close()
+                        queue[1]["ws"].close()
+                        queue.splice(0, 2)
+                    }
+                    else{
+                        const indexToRemove = (players[0]["accepted"]) ? indexB : indexA
+                        queue[indexToRemove]["ws"].close()
+                        // reset the confirmation of the other player
+                        queue[1 - indexToRemove]["ws"].send(JSON.stringify({"type": "MATCH_CONFIRMATION_RESET", "payload": ""}))
+                        queue.splice(indexToRemove, 1)
+                    }
+                    game_state = GAME_STATE.NOT_PLAYING
+                    players.splice(0, players.length) // clear array
                 }
-                game_state = GAME_STATE.NOT_PLAYING
-                players.splice(0, players.length) // clear array
             }
         }
+        confirmation_timer--
     }
     else if(game_state == GAME_STATE.PLAYING){
         // Check when timer reaches 0
