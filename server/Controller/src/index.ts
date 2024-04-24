d53fe22fca62720437ff64a1fb96ba89f456b5d1
Daniel Nguyen <94246004+CrazEpic@users.noreply.github.com>
2024-04-24 09:23:43 -0500

    Replace usages of username with user_id in Controller

--- server/Controller/src/index.ts
+++ server/Controller/src/index.ts
@@ -14,11 +14,11 @@ let CONTROLLER_ACCESS: string = ""
 
 // Temporary queue until Auth0+database is set up
 // ws to close connection on POST /removeuser
-const allowedUsers: Array<{"username": string, "user_id": string, "playernumber": number, "ws": any}> = []
+const allowedUsers: Array<{"user_id": string, "playernumber": number, "ws": any}> = []
 
 const printCurrentUsers = () => {
     let output = ""
-    allowedUsers.forEach((element) => {output += element["username"] + " "})
+    allowedUsers.forEach((element) => {output += element["user_id"] + " "})
     return output
 }
 
@@ -43,24 +43,23 @@ app.post("/accesspassword", (request, response) => {
 })
 
 app.post("/adduser", (request, response) => {
-    const username: string = request.body.username
     const user_id: string = request.body.user_id
     const playernumber: number = request.body.playernumber
     let status = 400
     // If allowedUsers.lenth < 2 and user is not in allowedUsers
-    if(allowedUsers.length < 2 && allowedUsers.findIndex((element) => {return element["username"] === username}) == -1){
+    if(allowedUsers.length < 2 && allowedUsers.findIndex((element) => {return element["user_id"] === user_id}) == -1){
         // ws is set after the player connects through WebSocket
-        allowedUsers.push({"username": username, "user_id": user_id, "playernumber": playernumber, "ws": null})
-        response.send(`ADDED PLAYER ${playernumber} as ${username}`)
-        console.log(`ADDED PLAYER ${playernumber} as ${username} | ALLOWED_USERS: ${printCurrentUsers()}`)
+        allowedUsers.push({"user_id": user_id, "playernumber": playernumber, "ws": null})
+        response.send(`ADDED PLAYER ${playernumber} as ${user_id}`)
+        console.log(`ADDED PLAYER ${playernumber} as ${user_id} | ALLOWED_USERS: ${printCurrentUsers()}`)
         status = 200
     }
     response.status(status).end()
 })
 
 app.post("/removeuser", (request, response) => {
-    const username: string = request.body.username
-    const index = allowedUsers.findIndex((element) => {return element["username"] === username})
+    const user_id: string = request.body.user_id
+    const index = allowedUsers.findIndex((element) => {return element["user_id"] === user_id})
     let status = 400
     // If user is in allowedUsers
     if(index != -1){
@@ -68,8 +67,8 @@ app.post("/removeuser", (request, response) => {
             allowedUsers[index]["ws"].close()
         }
         allowedUsers.splice(index, 1)
-        response.send(`REMOVED ${username}`)
-        console.log(`REMOVED ${username} | ALLOWED_USERS: ${printCurrentUsers()}`)
+        response.send(`REMOVED ${user_id}`)
+        console.log(`REMOVED ${user_id} | ALLOWED_USERS: ${printCurrentUsers()}`)
         status = 200
     }
     response.status(status).end()
