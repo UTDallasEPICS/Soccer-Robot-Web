705de5232b456c9f835da2836d49a4d2feb093e5
Surya Mallidi <surya_mallidi@outlook.com>
2024-04-24 15:11:49 -0500

    Converted the pug to html

--- components/SignUpOverlay.vue
+++ components/SignUpOverlay.vue
@@ -1,12 +1,17 @@
-<template lang="pug">
-div.fixed.w-full.h-full.inset-0.bg-black.bg-opacity-80.flex(style="z-index: 2")
-    div.w-min.h-min.rounded-lg.p-2.bg-white(style="margin: auto;")
-        div.w-min.h-min.text-black(style="margin-left: auto;")
-            p.cursor-pointer(@click="emitClose") X
-        p.text-black Sign Up
-        form(@submit.prevent="handleSubmit")
-            label.text-black Username
-            input.text-black.border-2.border-black(type="text" v-model="username" required)
+<template>
+<div class="fixed w-full h-full inset-0 bg-black bg-opacity-80 flex" style="z-index: 2;">
+    <div class="w-min h-min rounded-lg p-2 bg-white" style="margin: auto;">
+        <div class="w-min h-min text-black" style="margin-left: auto;">
+            <p class="cursor-pointer" @click="emitClose">X</p>
+        </div>
+        <p class="text-black">Sign Up</p>
+        <form @submit.prevent="handleSubmit">
+            <label class="text-black">Username</label>
+            <input class="text-black border-2 border-black" type="text" v-model="username" required>
+        </form>
+    </div>
+</div>
+
 </template>
 
 <script setup lang="ts">
