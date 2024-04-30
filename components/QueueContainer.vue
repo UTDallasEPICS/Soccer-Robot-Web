238aeb39600b738abf02dcc3c2cd5360db9b334c
Daniel Nguyen <94246004+CrazEpic@users.noreply.github.com>
2024-04-30 18:14:18 -0500

    Integrate updates for queue, score, and timer for frontend

--- components/QueueContainer.vue
+++ components/QueueContainer.vue
@@ -1,10 +1,12 @@
 <template>
-    <div class="w-340 h-full mt-[-12px]">
-      <UpComing/>
-      <Queue @join-queue="$emit('join-queue')" @leave-queue="$emit('leave-queue')"/>
-    </div>
-  </template>
-  
-  <script setup lang="ts">
-  </script>
-  
+<div class="w-340 h-full mt-[-12px]">
+  <UpComing/>
+  <Queue :queueUsers="queueUsers" @join-queue="$emit('join-queue')" @leave-queue="$emit('leave-queue')"/>
+</div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  queueUsers: {type: Array<string>, default: []},
+})
+</script>
