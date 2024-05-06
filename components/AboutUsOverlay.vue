26e508953272132165dc6c5b532021345ef3e01b
Megha Nettem <mnettem21@gmail.com>
2024-05-06 03:01:25 -0500

    About us, Help, How to play updates

--- components/AboutUsOverlay.vue
+++ components/AboutUsOverlay.vue
@@ -1,15 +1,20 @@
-<template lang="pug">
-div.fixed.w-full.h-full.inset-0.bg-black.bg-opacity-80.flex(style="z-index: 2")
-    div.rounded-lg.p-2.bg-white(style="width: 500px; height: 400px; margin: auto;")
-        div.w-min.h-min.text-black(style="margin-left: auto;")
-            p.cursor-pointer(@click="emitClose") X
-        p.w-full.text-center About Us
-        p Team Shawarama along with RL^2 presents to you: Soccer Robots! These fun, interactive robots allow you to play soccer from the comforts of your own phone. Using the WASD keys on your keyboard, you can move your robot to the World Cup! 
+<template>
+<div class="fixed w-full h-full inset-0 bg-black bg-opacity-80 flex" style="z-index: 2">
+      <div class="rounded-lg p-2 bg-white" style="width: 500px; height: 400px; margin: auto;">
+        <div class="w-min h-min text-black" style="margin-left: auto;">
+          <p @click="emitClose" class="cursor-pointer">X</p>
+        </div>
+        <p class="w-full text-center" style="font-size: 28px; margin-bottom: 1em;">About Us</p>
+        <p class="w-full text-center">Team Shawarama along with RL^2 presents to you: Soccer Robots! These fun, interactive robots allow you to play soccer from the comforts of your own phone. Using the WASD keys on your keyboard, you can move your robot to the World Cup!</p>
+        <img src="public/SoccerRobot.webp" alt="Soccer Robot Image" style="display: block; margin: 0 auto; width: 35%; height: auto; margin-top: .75em;">
+      </div>
+</div>
 </template>
-
-<script setup lang="ts">
-const emit = defineEmits(['closeAboutUsOverlay'])
-const emitClose = () => {
-    emit('closeAboutUsOverlay')
-}
-</script>
+  
+  <script setup lang="ts">
+  const emit = defineEmits(['closeAboutUsOverlay'])
+  const emitClose = () => {
+      emit('closeAboutUsOverlay')
+  }
+  </script>
+  
--- components/HowToPlayOverlay.vue
+++ components/HowToPlayOverlay.vue
@@ -1,15 +1,121 @@
-<template lang="pug">
-div.fixed.w-full.h-full.inset-0.bg-black.bg-opacity-80.flex(style="z-index: 2")
-    div.rounded-lg.p-2.bg-white(style="width: 500px; height: 400px; margin: auto;")
-        div.w-min.h-min.text-black(style="margin-left: auto;")
-            p.cursor-pointer(@click="emitClose") X
-        p.w-full.text-center(style="font-size: 28px;") How to Play
-        p Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia numquam atque sed dolorem laudantium minus ullam totam voluptatem hic doloremque aut temporibus impedit distinctio architecto eveniet quos sit ducimus, veniam tenetur qui consequuntur accusamus dolore consectetur. Atque mollitia quaerat deleniti rerum aliquid suscipit nulla facere quia pariatur at autem dolorem officia eius rem ea delectus non veniam facilis hic, doloribus, nisi dolor dicta illo iure. Dolorum ut expedita recusandae distinctio commodi nam accusantium. Accusamus eius repellendus distinctio at dolore ducimus? Natus, at vel? Quaerat, at, nemo repudiandae labore itaque dolorem provident sequi voluptatum nostrum quasi facere cum asperiores deleniti quisquam?
-</template>
-
-<script setup lang="ts">
-const emit = defineEmits(['closeHowToPlayOverlay'])
-const emitClose = () => {
+<template>
+    <div class="overlay">
+      <div class="content">
+        <!-- Top Content -->
+        <div class="top-content">
+          <div class="title-container">
+            <p class="title">How to Play</p>
+          </div>
+          <div class="close-button-container">
+            <p class="close-button" @click="emitClose">X</p>
+          </div>
+        </div>
+        <!-- Main Content -->
+        <div class="main-content">
+          <!-- Left Content -->
+          <div class="left-content">
+            <div class="text-content">
+              <p class="instructions">
+                <br>
+                <strong>Step 1:</strong> Register and Log In!<br>
+                <strong>Step 2:</strong> Join the Queue!<br>
+                <strong>Step 3:</strong> When your match starts, control your robot with the WASD keys. Defend the goal that matches your robot and score by getting the ball into the other color's goal.<br><br>
+                <strong>Did your Robot get stuck?</strong><br>
+                Someone will be by to un-stick the bots periodically. If the bots are stuck for a prolonged period of time, click "Help" and let us know!<br><br>
+                <strong>Don't see the soccer robots?</strong><br>
+                The arena is also our robot fighting arena! We're likely breaking things, so... please check back and try again later!<br><br>
+                <strong>The robots will return to their goals and charge themselves between matches as needed!</strong>
+              </p>
+            </div>
+          </div>
+          <!-- Right Content -->
+          <div class="right-content">
+            <br><br>
+            <img src="public/WASD.png" alt="W = Forward, A = Left, S = Right, D = Reverse" class="image">
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  const emit = defineEmits(['closeHowToPlayOverlay'])
+  const emitClose = () => {
     emit('closeHowToPlayOverlay')
-}
-</script>
+  }
+  </script>
+  
+  <style scoped>
+  .overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.8);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 2;
+  }
+  
+  .content {
+    background-color: white;
+    border-radius: 8px;
+    width: 800px;
+    height: 590px;
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+  }
+  
+  .top-content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  
+  .title-container {
+    flex-grow: 1; /* Take as much space as needed to center the title */
+    text-align: center;
+  }
+  
+  .close-button-container {
+    margin-left: auto; /* Push the close button to the right */
+  }
+  
+  .title {
+    font-size: 28px;
+    margin: 0;
+  }
+  
+  .close-button {
+    cursor: pointer;
+  }
+  
+  .main-content {
+    display: flex;
+  }
+  
+  .left-content {
+    flex: 1;
+  }
+  
+  .right-content {
+    margin-left: auto;
+  }
+  
+  .image {
+    max-width: 400px;
+    max-height: auto;
+  }
+  
+  .text-content {
+    max-width: 100%;
+  }
+  
+  .instructions {
+    word-wrap: break-word;
+  }
+  </style>
+  
